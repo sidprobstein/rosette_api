b121289ec1159f5c09e7988bd67a16aa4c15c11c
Sid <sid@rightwhen.com>
2016-01-02 14:21:45 -0500

    Revision V2
    This version saves responses and enriched versions, minimizing calls to
    Rosette and allowing you to customize the enrichment process

--- README.txt
+++ README.txt
@@ -2,21 +2,21 @@
 rosette-api
 ===========
 
-Python tools for use with the Basis Technology Rosette API entity extraction, categorization, sentiment analysis etc. 
+Python tools for use with the Basis Technology Rosette API
+
+enrich.py:  sends JSON documents to Rosette API and enriches them with entity extraction
+
 
 Usage
 -----
-
 python enrich.py [-h] [-o OUTPUTDIR] -k KEY filespec
 	
 
 Arguments
 ---------
-
+-h requests help
 -o specifies the directory where enriched files are written; the default is enriched
-
 -k specifies the rosette API key, and is required
-
 filespec must be the path to one or more json files 
 
 
--- src/rosette_api/enrich.py
+++ src/rosette_api/enrich.py
@@ -7,9 +7,6 @@
 @contact:    sid@rightwhen.com
 '''
 
-# to do : save the raw output to a file of similar name
-# to do: if the raw output exists, process it instead of calling rosette api
-
 import sys
 import os
 import argparse
@@ -22,8 +19,8 @@ from rosette.api import API, DocumentParameters
 def main(argv):
        
     parser = argparse.ArgumentParser(description='Enrich json files using the Rosette API')
-    parser.add_argument('-o', '--outputdir', default="enriched", help="subdirectory into which to place enriched files")
-    parser.add_argument('-d', '--debug', action="store_true", help="dump diagnostic information for debugging purposes")
+    parser.add_argument('-o', '--outputdir', default="enriched", help="subdirectory to store the enriched files")
+    parser.add_argument('-r', '--responsedir', default="responses", help="subdirectory to store Rosette responses")
     parser.add_argument('-k', '--key', required=True, help="rosette api key")
     parser.add_argument('filespec', help="path to the json file(s) to enrich")
     args = parser.parse_args()
@@ -39,119 +36,139 @@ def main(argv):
     else:
         sys.exit()
 
+    # create output directory
     sOutputDir = os.getcwd() + '/' + args.outputdir
-    print "outputdir:", sOutputDir
     if not os.path.isdir(sOutputDir):
-        print "mkdir:", sOutputDir
         try:
             os.mkdir(sOutputDir)
         except Exception, e:
             sys.exit(e)
 
+    # create response storage directory
+    sResponseDir = os.getcwd() + '/' + args.responsedir
+    if not os.path.isdir(sResponseDir):
+        try:
+            os.mkdir(sResponseDir)
+        except Exception, e:
+            sys.exit(e)
+
     for sFile in lstFiles:
         
-        print "opening:", sFile
-                           
+        # read the input file   
+        print "enrich.py: reading:", sFile                      
         try:
             f = open(sFile, 'r')
         except Exception, e:
-            sys.exit(e)
-        
-        ##########   
-        # read the file
+            print "enrich.py: error opening:", e
+            continue
         try:
-            jData = json.load(f)
+            jInput = json.load(f)
         except Exception, e:
-            sys.exit(e)
-                
-        sOutputFile = sOutputDir + '/' + sFile 
+            print "enrich.py: error reading:", e
+            f.close()
+            continue
         
-        if os.path.isfile(sOutputFile):
-            print "skipping:", sFile, "(already enriched)"
+        # ASSERT: jInput is valid and populated
+
+        # construct related file paths                
+        sOutputFile = sOutputDir + '/' + sFile 
+        sResponseFile = sResponseDir + '/' + sFile 
+        
+        # if there is an existing response file...
+        if os.path.isfile(sResponseFile):
+            print "enrich.py: using stored response:", sResponseFile
+            try:
+                fi = open(sResponseFile, 'r')
+            except Exception, e:
+                print "enrich.py: error opening:", e
+                f.close()
+                continue
+            try:
+                jResponse = json.load(fi)
+            except Exception, e:
+                print "enrich.py: error reading:", e
+                fi.close()
+                f.close()
+                continue
+            # jResponse now contains the stored response file...
         else:
-            # combine
-            sText = jData['subject'] + ' ' + jData['body']
-            
-            ##########
-            # enrich
-            
-            # connect to rosette if not already done
+            print "enrich.py: sending text to Rosette API..."
+            # extract text from the file
+            sText = jInput['subject'] + ' ' + jInput['body']
+            # to do: strip ascii etc?
+            # connect to rosette 
             if not rosette:
                 rosette = API(user_key=args.key)
                 params = DocumentParameters()
-            
-            ##########
-            # entities
+            # send the text
             params['content'] = sText
-            result = rosette.entities(params)  # entity linking is turned off
+            jResponse = rosette.entities(params)  # entity linking is turned off
+            # write out the response
+            print "enrich.py: writing response:", sResponseFile
+            try:
+                fr = open(sResponseFile, 'w')
+            except Exception, e:
+                print "enrich.py: error creating:", e
+                f.close()
+                continue
+            try:
+                json.dump(jResponse,fr, sort_keys=True, indent=4, separators=(',', ': '))
+            except Exception, e:
+                print "enrich.py: error writing:", e
+                fr.close()
+                f.close()
+                continue
+            # the response file has been written
+            fr.close()
+        
+        # ASSERT: jResponse is valid and populated
             
-            if args.debug:
-                print result
-            
-            # filter, invert
-            lstUniques = []
-            lstEntities = result['entities']
+        # if there is an enriched file...
+        if os.path.isfile(sOutputFile):
+            print "enrich.py: already enriched:", sOutputFile
+        else:
+            # filter and invert the response
+            dictEntities = {}
+            lstEntities = jResponse['entities']
             for entity in lstEntities:
                 if float(entity['confidence']) > 0.01:
                     # map the type
                     sType = entity['type'].lower()
                     if ':' in sType:
                         sType = sType.split(':')[1]
-                    if jData.has_key(sType):
-                        # add entity
-                        lstUniques.append(jData[sType])
-                        lstUniques.append(entity['normalized'])
-                        jData[sType] = lstUniques
+                    # enrich the input file JSON structure:
+                    lstTmp = []
+                    if dictEntities.has_key(sType):
+                        # add entity to type
+                        lstTmp = dictEntities[sType]
+                        lstTmp.append(entity['normalized'])
+                        dictEntities[sType] = lstTmp
                     else:
                         # add type
-                        jData[sType] = entity['normalized']
-            
-            ##########
-            # categories
-            result = rosette.categories(params)  # entity linking is turned off
+                        lstTmp.append(entity['normalized'])
+                        dictEntities[sType] = lstTmp
+                        
+            for k in dictEntities.iterkeys():
+                jInput[k] = dictEntities[k]
 
-            if args.debug:
-                print result
-
-            # filter, invert
-            lstUniques = []
-            lstCategories = result['categories']
-            for category in lstCategories:    
-                if float(category['confidence']) > 0.5:
-                    lstUniques.append(category['label'].lower())
-            if len(lstUniques) > 0:
-                jData['categories'] = lstUniques
-
-            ##########
-            # sentiment
-            result = rosette.sentiment(params)  # entity linking is turned off
-
-            if args.debug:
-                print result
-            
-            # filter, invert
-            lstSentiment = result['sentiment']
-            for sentiment in lstSentiment:    
-                if float(sentiment['confidence']) > 0.61:
-                    jData[u'sentiment'] = sentiment['label']
-                    
-            ##########
-            # write
-            
-            print "creating:", sOutputFile
+            # write the enriched file
+            print "enrich.py: writing:", sOutputFile
             try:
                 fo = open(sOutputFile, 'w')
-            except Exception, e:                # to do: clean up
-                f.close()
-                sys.exit(e)
-            print "writing:", sOutputFile
-            # write the file
-            try:
-                json.dump(jData,fo, sort_keys=True, indent=4, separators=(',', ': '))
             except Exception, e:
-                sys.exit(e)
+                print "enrich.py: error creating:", e
+                f.close()
+                continue
+            try:
+                json.dump(jInput,fo, sort_keys=True, indent=4, separators=(',', ': '))
+            except Exception, e:
+                print "enrich.py: error writing:", e
+                fo.close()
+                f.close()
+                continue
             fo.close()
-                        
+            
+        # close input file
         f.close()
                         
     # end for
